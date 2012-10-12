f157936ca50bab08c229355f4cab5b822451ed8f
lint <lintao@ihep.ac.cn>
2012-10-12 11:15:22 +0800

    Add Test.

--- Geant4Project/HepEvtGenerator/test/src/ParticleInfoTest.cc
+++ Geant4Project/HepEvtGenerator/test/src/ParticleInfoTest.cc
@@ -8,6 +8,10 @@ void
 ParticleInfoTest::testParticleInfo()
 {
   Generator::Utils::ParticleInfo gu_pi;
+  gu_pi . pid = 22;
+  gu_pi . px = 1.216710e-03 * GeV;
+  gu_pi . py = 6.703129e-04 * GeV;
+  gu_pi . pz = -4.525696e-04 * GeV;
 }
 
 }
