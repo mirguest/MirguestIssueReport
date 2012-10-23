cbcd9e9d29957d1e0cbe19ec81accc248aefc1b9
lint <lintao@ihep.ac.cn>
2012-10-23 20:24:35 +0800

    Learn here document.

---
+++ Prog/shell/heredocument.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Refer: http://en.wikipedia.org/wiki/Here_document
+# Refer: http://serverfault.com/questions/72476/clean-way-to-write-complex-multi-line-string-to-a-variable
+var=`python << EOF
+print "echo 1 ;"
+print "\n"
+print "echo 2 ;"
+EOF
+`
+echo $var
+$var
