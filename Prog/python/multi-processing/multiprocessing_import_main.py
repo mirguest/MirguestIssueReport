ecae21b25bc0a79659f8e1441e57bf6ec4ef5265
lint <lintao@ihep.ac.cn>
2013-01-29 20:44:56 +0800

    import worker.

---
+++ Prog/python/multi-processing/multiprocessing_import_main.py
@@ -0,0 +1,9 @@
+import multiprocessing
+import multiprocessing_import_worker
+
+if __name__ == '__main__':
+    jobs = []
+    for i in range(5):
+        p = multiprocessing.Process(target=multiprocessing_import_worker.worker)
+        jobs.append(p)
+        p.start()
