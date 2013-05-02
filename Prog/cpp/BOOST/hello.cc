9bb572b4846006541ebd079e3fbd39b115d8a168
lintao <lintao@ihep.ac.com>
2013-05-02 15:02:57 +0800

    fisrt step.

--- Prog/cpp/BOOST/hello.cc
+++ Prog/cpp/BOOST/hello.cc
@@ -1,7 +1,20 @@
+#include <string>
+
 char const* greet()
 {
        return "hello, world";
 }
+
+struct World
+{
+    void set(std::string msg) {
+        this->msg = msg;
+    }
+    std::string greet() {
+        return msg;
+    }
+    std::string msg;
+};
 
 #include <boost/python.hpp>
 using namespace boost::python;
@@ -9,4 +22,9 @@ using namespace boost::python;
 BOOST_PYTHON_MODULE(hello)
 {
     def("greet", greet);
+
+    class_<World>("World")
+        .def("greet", &World::greet)
+        .def("set", &World::set)
+    ;
 }
