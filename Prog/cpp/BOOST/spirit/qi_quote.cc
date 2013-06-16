d6b4c6771f619288683a2b438b0a295ca08823e2
lint <lintao@ihep.ac.cn>
2013-06-16 17:47:31 +0800

    more test.

--- Prog/cpp/BOOST/spirit/qi_quote.cc
+++ Prog/cpp/BOOST/spirit/qi_quote.cc
@@ -5,20 +5,14 @@
 
 using namespace boost::spirit;
 
-int main() {
-
-    std::string str1("'hello, world'");
-    std::string str2("\"hello, world\"");
-    std::string str3("'hello,\" world'");
-    std::string str4("'hello,\\\' world'");
+bool quick_parse(std::string str) {
 
     qi::rule<std::string::iterator, 
              std::string(), 
              ascii::space_type> quoted_string;
-    char qq;
     quoted_string %= qi::lexeme[
                      (qi::lit("'")
-                  >> +("\\\'"|(ascii::char_ - qi::lit("'")))
+                  >> +("\\'"|(ascii::char_ - qi::lit("'")))
                   >> qi::lit("'"))
                      |
                      (qi::lit("\"")
@@ -27,46 +21,46 @@ int main() {
                      ];
 
     std::string result;
-
     std::string::iterator strbegin;
 
-    result.clear();
-    strbegin = str1.begin();
-    bool r = qi::phrase_parse(strbegin, str1.end(),
+    strbegin = str.begin();
+    bool r = qi::phrase_parse(strbegin, str.end(),
             quoted_string,
             ascii::space,
             result);
-    std::cout << "str1: " << str1 << std::endl;
+    std::cout << "----------------------" << std::endl;
+    std::cout << "string: " << str << std::endl;
     std::cout << r << std::endl;
     std::cout << result << std::endl;
+    std::cout << "----------------------" << std::endl;
 
-    result.clear();
-    strbegin = str2.begin();
-    r = qi::phrase_parse(strbegin, str2.end(),
-            quoted_string,
-            ascii::space,
-            result);
-    std::cout << "str2: " << str2 << std::endl;
-    std::cout << r << std::endl;
-    std::cout << result << std::endl;
+}
 
-    result.clear();
-    strbegin = str3.begin();
-    r = qi::phrase_parse(strbegin, str3.end(),
-            quoted_string,
-            ascii::space,
-            result);
-    std::cout << "str3: " << str3 << std::endl;
-    std::cout << r << std::endl;
-    std::cout << result << std::endl;
+int main() {
 
-    result.clear();
-    strbegin = str4.begin();
-    r = qi::phrase_parse(strbegin, str4.end(),
-            quoted_string,
-            ascii::space,
-            result);
-    std::cout << "str4: " << str4 << std::endl;
-    std::cout << r << std::endl;
-    std::cout << result << std::endl;
+    // no quote
+    // single quote
+    std::string str_s_1("'hello, world'");
+    std::string str_s_2("'hello,\" world'");
+    std::string str_s_3("'hello,\\\" world'");
+    std::string str_s_4("'hello,\\' world'");
+    std::string str_s_5("'hello,\\\' world'");
+    // double quote
+    std::string str_d_1("\"hello, world\"");
+    std::string str_d_2("\"hello,\" world\"");
+    std::string str_d_3("\"hello,\\\" world\"");
+    std::string str_d_4("\"hello,\' world\"");
+    std::string str_d_5("\"hello,\\\' world\"");
+
+    quick_parse(str_s_1);
+    quick_parse(str_s_2);
+    quick_parse(str_s_3);
+    quick_parse(str_s_4);
+    quick_parse(str_s_5);
+    quick_parse(str_d_1);
+    quick_parse(str_d_2);
+    quick_parse(str_d_3);
+    quick_parse(str_d_4);
+    quick_parse(str_d_5);
+
 }
