e84d347e3472bc3723b4deee9bcbc86c78462570
lint <lintao@ihep.ac.cn>
2013-06-16 15:07:00 +0800

    test s1 ref.

--- Prog/cpp/BOOST/xpressive/hello.cc
+++ Prog/cpp/BOOST/xpressive/hello.cc
@@ -9,6 +9,7 @@ int main()
 
     //sregex rex = sregex::compile( "(\\w+) (\\w+)!" );
     sregex rex = (s1= +_w) >> ' ' >> (s2= +_w) >> '!';
+    sregex rex2 = (s1= +_w) >> ' ' >> (s1) >> '!';
 
     smatch what;
 
@@ -17,6 +18,13 @@ int main()
         std::cout << what[0] << '\n'; // whole match
         std::cout << what[1] << '\n'; // first capture
         std::cout << what[2] << '\n'; // second capture
+    }
+
+    smatch what2;
+    std::string world("world world!");
+    if (regex_match(world, what2, rex2)) {
+        std::cout << what2[0] << '\n'; // whole match
+        std::cout << what2[1] << '\n'; // first capture
     }
 
     return 0;
