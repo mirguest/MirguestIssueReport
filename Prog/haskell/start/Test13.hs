187181d675c5af387f329d94fb888f6b109c8e6e
lint <lintao@ihep.ac.cn>
2013-09-22 19:17:51 +0800

    Add maximum.

--- Prog/haskell/start/Test13.hs
+++ Prog/haskell/start/Test13.hs
@@ -2,3 +2,11 @@ max' :: (Ord a) => a -> a -> a
 max' a b
     | a > b = a
     | otherwise = b
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
