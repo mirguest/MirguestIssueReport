5c5fee5b360caf0e19a59aa3985f6022fda9c9e7
lint <lintao@ihep.ac.cn>
2013-09-23 08:58:11 +0800

    Add zip with

--- Prog/haskell/start/Test18.hs
+++ Prog/haskell/start/Test18.hs
@@ -2,3 +2,8 @@
 
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
