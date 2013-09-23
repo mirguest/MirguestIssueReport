13aa0828dd3852c1f2f01772082d9508ba19c17e
lint <lintao@ihep.ac.cn>
2013-09-23 09:43:12 +0800

    add flip

--- Prog/haskell/start/Test18.hs
+++ Prog/haskell/start/Test18.hs
@@ -7,3 +7,7 @@ zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
 zipWith' _ [] _ = []
 zipWith' _ _ [] = []
 zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
