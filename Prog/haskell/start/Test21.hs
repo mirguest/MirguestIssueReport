778c2d01ec650f0eb8195c9746dbb79580dbf891
lint <lintao@ihep.ac.cn>
2013-09-25 10:32:20 +0800

    Add nudge.

--- Prog/haskell/start/Test21.hs
+++ Prog/haskell/start/Test21.hs
@@ -7,3 +7,9 @@ surface :: Shape -> Float
 surface (Circle _ r) = pi * r^2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) 
     = (abs $ x2-x1) * (abs $ y2-y1)
+
+nudge :: Shape -> Float -> Float -> Shape
+nudge (Circle (Point x y) r) a b
+    = Circle (Point (x+a) (y+b)) r
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b
+    = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
