01a6fbe59dd369fb38f65ca47f9f1d81331ceba8
lint <lintao@ihep.ac.cn>
2013-09-25 10:08:54 +0800

    add the Point.

--- Prog/haskell/start/Test21.hs
+++ Prog/haskell/start/Test21.hs
@@ -1,7 +1,9 @@
-data Shape = Circle Float Float Float 
-             | Rectangle Float Float Float Float
+data Point = Point Float Float deriving (Show)
+data Shape = Circle Point Float 
+             | Rectangle Point Point
              deriving (Show)
 
 surface :: Shape -> Float
-surface (Circle _ _ r) = pi * r^2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2-x1) * (abs $ y2-y1)
+surface (Circle _ r) = pi * r^2
+surface (Rectangle (Point x1 y1) (Point x2 y2)) 
+    = (abs $ x2-x1) * (abs $ y2-y1)
