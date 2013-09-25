2083417576ba74e41168001927aa5520ea58e10a
lint <lintao@ihep.ac.cn>
2013-09-25 13:45:54 +0800

    auxiliary funs.

--- Prog/haskell/start/Test21.hs
+++ Prog/haskell/start/Test21.hs
@@ -13,3 +13,9 @@ nudge (Circle (Point x y) r) a b
     = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b
     = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
+baseRect :: Float -> Float -> Shape
+baseRect width height = Rectangle (Point 0 0) (Point width height)
