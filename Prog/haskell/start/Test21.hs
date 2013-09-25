668c7c956b4e51a058932c354986f0ad6f541c89
lint <lintao@ihep.ac.cn>
2013-09-25 10:03:38 +0800

    Add deriving show.

--- Prog/haskell/start/Test21.hs
+++ Prog/haskell/start/Test21.hs
@@ -1,5 +1,6 @@
 data Shape = Circle Float Float Float 
              | Rectangle Float Float Float Float
+             deriving (Show)
 
 surface :: Shape -> Float
 surface (Circle _ _ r) = pi * r^2
