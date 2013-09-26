9ceb0f2d76ded15ef2e807ddc5f013b21186ac0a
lint <lintao@ihep.ac.cn>
2013-09-26 09:49:23 +0800

    add .++

--- Prog/haskell/start/Test24.hs
+++ Prog/haskell/start/Test24.hs
@@ -1,2 +1,7 @@
 infixr 5 :-:
 data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
