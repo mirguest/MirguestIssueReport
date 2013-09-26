642ca4735ad2819030a2071810c45abcd769ef89
lint <lintao@ihep.ac.cn>
2013-09-26 11:09:25 +0800

    insert element into tree.

--- Prog/haskell/start/Test25.hs
+++ Prog/haskell/start/Test25.hs
@@ -5,3 +5,10 @@ data Tree a =
 
 singleton :: a -> Tree a
 singleton x = Node x EmptyTree EmtryTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmtryTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
