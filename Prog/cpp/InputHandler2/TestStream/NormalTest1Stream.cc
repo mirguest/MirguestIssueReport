f55c3e5f99d52dfacbdce1425ce25cd704700743
lint <lintao@ihep.ac.cn>
2014-04-16 10:49:35 +0800

    Load Tree library.

--- Prog/cpp/InputHandler2/TestStream/NormalTest1Stream.cc
+++ Prog/cpp/InputHandler2/TestStream/NormalTest1Stream.cc
@@ -1,5 +1,17 @@
-
+#include <iostream>
 #include "TestStream/NormalTest1Stream.h"
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TSystem.h"
+
+NormalTest1Stream::NormalTest1Stream() {
+    m_index = -1;
+    m_path = "";
+
+    m_file = 0;
+    m_tree = 0;
+}
 
 bool
 NormalTest1Stream::next() {
@@ -14,5 +26,23 @@ NormalTest1Stream::get(const std::string& objPath) {
 
 bool
 NormalTest1Stream::attach(const std::string& filepath) {
+    std::cout << __LINE__ << " " << filepath << std::endl;
+    m_path = filepath;
+    return init_tree();
+}
+
+bool
+NormalTest1Stream::init_tree() {
+    gSystem->Load("libTree.so");
+    m_file = new TFile(m_path.c_str());
+
+    if (not m_file) {
+        return false;
+    }
+
+    m_tree = (TTree*) m_file->Get("evt");
+    if (not m_tree) {
+        return false;
+    }
     return true;
 }
--- Prog/cpp/InputHandler2/TestStream/NormalTest1Stream.h
+++ Prog/cpp/InputHandler2/TestStream/NormalTest1Stream.h
@@ -1,15 +1,30 @@
 #ifndef NormalTest1Stream_h
 #define NormalTest1Stream_h
 
+#include <string>
 #include "IStream.h"
+
+class TFile;
+class TTree;
 
 class NormalTest1Stream: public IStream {
 public:
+    NormalTest1Stream();
+
     bool next();
     EventObject* get(const std::string& objPath);
     // attach input files
     bool attach(const std::string& filepath);
 
+private:
+    bool init_tree();
+
+private:
+    int m_index;
+    std::string m_path;
+
+    TFile* m_file;
+    TTree* m_tree;
 };
 
 #endif
