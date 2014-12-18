fdbad5bcab99c3ab64d19a3dc99cba2bf64f34c5
Tao Lin <lintao51@gmail.com>
2014-12-18 14:19:01 +0800

    add runtime error handler.

--- Prog/cpp/BOOST/system/copyfile.cc
+++ Prog/cpp/BOOST/system/copyfile.cc
@@ -3,12 +3,15 @@
  */
 
 #include <boost/filesystem.hpp>
+#include <exception>
 
 int main() {
     std::string from_filename("hello.txt");
     std::string to_filename("exist.txt");
     try {
         boost::filesystem::copy_file( from_filename, to_filename );
+    } catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;
     } catch (...) {
 
     }
