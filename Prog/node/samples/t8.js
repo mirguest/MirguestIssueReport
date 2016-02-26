d67c0442124364e27c39d1521eb0004c230c3ed5
Tao Lin <lintao51@gmail.com>
2016-02-26 22:57:53 +0800

    HTTP CLIENT (Exercise 7 of 13)

---
+++ Prog/node/samples/t8.js
@@ -0,0 +1,10 @@
+
+http = require('http')
+
+var url = process.argv[2]
+
+http.get(url, function (response) {
+    response.on("data", function (data) {
+        console.log(data.toString())
+    })
+})
