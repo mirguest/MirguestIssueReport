e54039d491c23289e900deb5d95acd071fd4e2d9
Tao Lin <lintao51@gmail.com>
2016-02-28 22:14:36 +0800

    show how to suppress help options.

--- Prog/python/argparse/t.py
+++ Prog/python/argparse/t.py
@@ -2,21 +2,24 @@
 # -*- coding:utf-8 -*-
 # author: lintao
 
+import sys
+helpmore = False
+if '--help-more' in sys.argv:
+    helpmore = True
+
 import argparse
 
 parser = argparse.ArgumentParser(description='Run JUNO Detector Simulation.')
 parser.add_argument("--evtmax", type=int, default=10, help='events to be processed')
 parser.add_argument("--help-more", action='store_true')
-helpmore = parser.parse_args()
-print helpmore
 # put hide options
 def h(helpstr):
-    if helpmore.help_more:
+    if helpmore:
         return helpstr
-    return argparse.SUPRESS
+    return argparse.SUPPRESS
 parser.add_argument("--mac", default="run.mac", help=h("mac "))
 
 
 args = parser.parse_args()
-if args.help_more:
+if '--help-more' in sys.argv:
     parser.print_help()
