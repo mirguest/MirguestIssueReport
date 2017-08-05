aac7c6c4dadfc5c377e106b32758e18b446c62e6
Tao Lin <lintao51@gmail.com>
2017-08-05 15:35:25 +0800

    failed to use objName.

--- Prog/cpp/c++11/thread/my_wait.cc
+++ Prog/cpp/c++11/thread/my_wait.cc
@@ -43,9 +43,9 @@ void signals()
  
 int main()
 {
-    std::thread t1(waits), t2(waits), t3(waits), t4(signals);
+    std::thread t1(waits), t4(signals), t2(waits), t3(waits);
+    t4.join();
     t1.join(); 
     t2.join(); 
     t3.join();
-    t4.join();
 }
---
+++ Prog/cpp/c++11/typetraits/is_static_method.cc
@@ -0,0 +1,97 @@
+/*
+ * g++ -std=c++11 is_static_method.cc
+ */
+
+#include <type_traits>
+#include <iostream>
+namespace SniperLog
+{
+    int LogLevel = 3;
+    //2:debug, 3:info, 4:warn, 5:error, 6:fatal
+    int logLevel() { 
+        return LogLevel; 
+    }
+
+    //return the global scope and objName string
+    const std::string& scope() {
+	static const std::string NonScope = "SNiPER:";
+	return NonScope;
+    }
+    const std::string& objName() {
+        static const std::string NonName = "NonName";
+	return NonName;
+    }
+
+    class LogHelper
+    {
+    public :
+
+	LogHelper(int flag,
+		  int level,
+		  const std::string& scope,
+		  const std::string& objName,
+		  const char* func
+		  )
+	    : m_active(flag >= level)
+	{
+	}
+
+	~LogHelper() {
+        }
+
+	LogHelper& operator<<(std::ostream& (*_f)(std::ostream&)) {
+	    return *this;
+        }
+
+	template<typename T>
+	LogHelper& operator<<(const T& t)
+	{
+	    if ( m_active ) std::cout << t;
+	    return *this;
+	}
+
+
+        private :
+
+            const bool m_active;
+    };
+
+}
+
+using SniperLog::logLevel;
+using SniperLog::scope;
+using SniperLog::objName;
+
+
+#define SNIPERLOG(Flag)  SniperLog::LogHelper(\
+        Flag,\
+        logLevel(),\
+        scope(),\
+        objName(),\
+        __func__\
+        )
+
+#define LogTest   SNIPERLOG(0)
+#define LogDebug  SNIPERLOG(2)
+#define LogInfo   SNIPERLOG(3)
+#define LogWarn   SNIPERLOG(4)
+#define LogError  SNIPERLOG(5)
+#define LogFatal  SNIPERLOG(6)
+
+
+
+class A {
+public:
+    static void mymethod() {
+        LogInfo << "hello" << std::endl;
+    }
+
+    const std::string& objName() { return m_name; }
+
+private:
+    std::string m_name;
+};
+
+int main() {
+    A::mymethod();
+}
