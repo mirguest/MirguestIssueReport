fdb24bcee1bf37ff6dee6c2e653e2aafaacab9d3
Tao Lin <lintao@ihep.ac.cn>
2021-01-10 16:19:02 +0800

    props in function component.

--- coding/webapp/react/myfirstreact/src/App.js
+++ coding/webapp/react/myfirstreact/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import props from 'prop-types';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello, {props.name} !</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
