4607b0004d996b1594c35e618de0ff26e250ea96
Tao Lin <lintao51@gmail.com>
2022-07-14 22:42:01 +0800

    add Makefile.

--- coding/fortran/c/myimplementation.f90
+++ coding/fortran/c/myimplementation.f90
@@ -1,7 +1,7 @@
 subroutine finit() bind(C, name="finit")
-
+  write(*,*) "INIT"
 end subroutine finit
 
 subroutine fexecute() bind(C, name="fexecute")
-
+  write(*,*) "EXECUTE"
 end subroutine fexecute
