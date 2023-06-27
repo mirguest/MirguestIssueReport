1f27cc105b5fdac2ecd7b4ab7dc6d9bb73d84b09
Tao Lin <lintao51@gmail.com>
2023-06-28 07:56:40 +0800

    update.

--- coding/node/checkin/main.js
+++ coding/node/checkin/main.js
@@ -4,14 +4,31 @@ const checkin = async() => {
     const apiurl = process.env.APIURL;
     if (!apiurl) return;
 
+    const referer = process.env.REFERER;
+    if (!referer) return;
+
     const cookie = process.env.COOKIE;
     if (!cookie) return;
+
+    const body = process.env.BODY;
+    if (!body) return;
     
 
     console.log(`apiurl ${apiurl}`);
     console.log(`cookie ${cookie}`);
+    console.log(`body ${body}`);
 
-    
+    const checkin_result = await fetch(apiurl, {
+        method: 'POST',
+        headers: {
+            'cookie': cookie,
+            'referer': referer,
+            'user-agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
+            'content-type': 'application/json'},
+        body: body,
+    }).then((r) => r.json());
+
+    console.log(`checkin result: ${checkin_result.message}`);
 }
 
 const main = async () => {
