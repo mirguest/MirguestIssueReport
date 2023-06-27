1b0a5619dbfecd3a1c7b3595b700fbad659041fb
Tao Lin <lintao51@gmail.com>
2023-06-28 07:38:13 +0800

    update.

--- coding/node/checkin/main.js
+++ coding/node/checkin/main.js
@@ -1,6 +1,22 @@
-const main = () => {
+require('dotenv').config();
 
+const checkin = async() => {
+    const apiurl = process.env.APIURL;
+    if (!apiurl) return;
+
+    const cookie = process.env.COOKIE;
+    if (!cookie) return;
+    
+
+    console.log(`apiurl ${apiurl}`);
+    console.log(`cookie ${cookie}`);
+
+    
 }
 
-main()
+const main = async () => {
+    await checkin();
+}
+
+main();
 
