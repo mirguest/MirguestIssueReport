e78518c235cb8eb03fd3b360a710eb0169bcb134
Tao Lin <lintao51@gmail.com>
2023-07-03 13:49:38 +0800

    update.

--- coding/go/learn/hello.go
+++ coding/go/learn/hello.go
@@ -11,10 +11,12 @@ package main
 import (
     "fmt"
     "os"
+    "rsc.io/quote"	
 )
 
 // Let us start
 func main() {
     fmt.Println("Hello, world!")  // Print simple text on screen
     fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable
+    fmt.Println(quote.Go())
 }
