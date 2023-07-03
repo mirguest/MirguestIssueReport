cf1eb7feb34bf5738a756cbf89abf48fb6f51ec9
Tao Lin <lintao51@gmail.com>
2023-07-03 17:14:38 +0800

    format using go format.

--- coding/go/learn/hello.go
+++ coding/go/learn/hello.go
@@ -10,30 +10,30 @@
 //
 // Download dependencies:
 // $ go mod tidy
-// 
+//
 // Hello Word in Go
 package main
 
 // Import OS and fmt packages
 import (
-    "fmt"
-    "log"
-    "os"
-    "rsc.io/quote"	
-    "mirguest/go/learn/greetings"
+	"fmt"
+	"log"
+	"mirguest/go/learn/greetings"
+	"os"
+	"rsc.io/quote"
 )
 
 // Let us start
 func main() {
-    fmt.Println("Hello, world!")  // Print simple text on screen
-    fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable
-    fmt.Println(quote.Go())
-    message, err := greetings.Hello("")
+	fmt.Println("Hello, world!")                          // Print simple text on screen
+	fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable
+	fmt.Println(quote.Go())
+	message, err := greetings.Hello("")
 
-    if err != nil {
-	log.Print(err)
-    }
+	if err != nil {
+		log.Print(err)
+	}
 
-    message, err = greetings.Hello(os.Getenv("USER"))
-    fmt.Println(message)
+	message, err = greetings.Hello(os.Getenv("USER"))
+	fmt.Println(message)
 }
