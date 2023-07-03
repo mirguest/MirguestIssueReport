f5c59e1f750be23b8fa0b1bd6d2b537ca923dc1d
Tao Lin <lintao51@gmail.com>
2023-07-03 12:33:56 +0800

    add hello.go

---
+++ coding/go/learn/hello.go
@@ -0,0 +1,20 @@
+// Run
+// $ go run hello.go
+// Build
+// $ go build hello.go
+// $ ./hello
+// 
+// Hello Word in Go
+package main
+
+// Import OS and fmt packages
+import (
+    "fmt"
+    "os"
+)
+
+// Let us start
+func main() {
+    fmt.Println("Hello, world!")  // Print simple text on screen
+    fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable
+}
