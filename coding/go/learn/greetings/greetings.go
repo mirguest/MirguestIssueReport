95ef7c964ebb8e7ea120be889f384be826ba3f1f
Tao Lin <lintao51@gmail.com>
2023-07-03 17:16:27 +0800

    format using go fmt.

--- coding/go/learn/greetings/greetings.go
+++ coding/go/learn/greetings/greetings.go
@@ -1,15 +1,16 @@
 package greetings
 
 import (
-    "fmt"
-    "errors"
+	"errors"
+	"fmt"
 )
+
 // Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-    if name == "" {
-        return "", errors.New("empty name")
-    }
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message, nil
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil
 }
