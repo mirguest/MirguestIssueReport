f94a5d63b57bf7966cfd876548420c960a1574eb
Tao Lin <lintao51@gmail.com>
2023-07-03 17:06:46 +0800

    update.

--- coding/go/learn/greetings/greetings.go
+++ coding/go/learn/greetings/greetings.go
@@ -1,10 +1,15 @@
 package greetings
 
-import "fmt"
-
+import (
+    "fmt"
+    "errors"
+)
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+    if name == "" {
+        return "", errors.New("empty name")
+    }
     // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+    return message, nil
 }
--- coding/go/learn/hello.go
+++ coding/go/learn/hello.go
@@ -17,6 +17,7 @@ package main
 // Import OS and fmt packages
 import (
     "fmt"
+    "log"
     "os"
     "rsc.io/quote"	
     "mirguest/go/learn/greetings"
@@ -27,5 +28,12 @@ func main() {
     fmt.Println("Hello, world!")  // Print simple text on screen
     fmt.Println(os.Getenv("USER"), ", Let's be friends!") // Read Linux $USER environment variable
     fmt.Println(quote.Go())
-    fmt.Println(greetings.Hello(os.Getenv("USER")))
+    message, err := greetings.Hello("")
+
+    if err != nil {
+	log.Print(err)
+    }
+
+    message, err = greetings.Hello(os.Getenv("USER"))
+    fmt.Println(message)
 }
